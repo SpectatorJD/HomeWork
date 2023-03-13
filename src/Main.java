230ccaf4ba2408c2574932df29704e45834db1bf
Tkakishak <tkakishakd@mail.ru>
2023-03-13 22:02:54 +0300

    HomeWork0313

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,47 @@
+import static com.sun.tools.javac.util.StringUtils.toUpperCase;
+
 public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println( "ФИО сотрудника " + fullName);
+
 
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+        String firstName = "Ivanov ";
+        String middleName = "Ivan ";
+        String lastName = "Ivanovich";
+        String fullName = firstName + middleName + lastName;
+        System.out.println("ФИО сотрудника " + toUpperCase(fullName));
+
+    }
+
+    public static void task3 () {
+        System.out.println("Задача 3");
+        String firstName = "Иванов ";
+        firstName = firstName.replace("е", "ё");
+
+        String middleName = "Семен ";
+        middleName = middleName.replace( "Семен ", "Семён ");
+
+        String lastName = "Семенович";
+        lastName = lastName.replace( "Семенович", "Семёнович");
+
+        String fullName = firstName + middleName + lastName;
+
+        System.out.println(fullName);
 
     }
 }
